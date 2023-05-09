7743d65f66198fa41047575972344310b0aca0e9
atarashiiJS <nduytan.uit.a3@gmail.com>
2023-05-09 16:46:59 +0700

    Initial commit

---
+++ frontend/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h3>HEADER</h3>
+    </div>
+</template>
+<script lang="ts">
+export default {
+    name: 'Header'
+}
+</script>
---
+++ frontend/layouts/defaut.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <Header/>
+        <navigation  position="fixed"/>
+        <main class="container mx-auto">
+            <slot />
+
+        </main>
+        <Footer/>
+    </div>
+</template>
+<script>
+import Footer from '~/components/Footer.vue';
+import Header from '~/components/Header.vue';
+
+
+export default {
+    name: 'default',
+    components: {
+        Header,
+        Footer
+    }
+}
+
+</script>
